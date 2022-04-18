06c253b12cf830863ac9482f66579bd39467f8ba
unknown <ZX@FZ-LINFEI-YE.fzzixun.com>
2022-04-18 14:22:42 +0800

    修改注册表

--- reg.py
+++ reg.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 import winreg
 
+
 def createId(path):
     sha = hashlib.sha256(str(path).encode('utf-16le')).hexdigest()
     prefix = sha[:32]
@@ -12,8 +13,9 @@ def createId(path):
     for old_char in prefix:
         code = int(old_char, 16)
         pluginId += chr(ord_a + code)
-    
+
     return pluginId
+
 
 id = createId(os.getcwd() + "\ext")
 print(os.getcwd())
@@ -25,9 +27,7 @@ data["allowed_origins"][0] = "chrome-extension://" + id + "/"
 with open("manifest.json", "w") as file:
     json.dump(data, file, ensure_ascii=False)
 
-# manifest = os.getcwd() + '\manifest.json'
-# subkey = r'SOFTWARE\Google\Chrome\NativeMessagingHosts\zrpa.chrome.bridge'
-
-# winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, subkey)
-# winreg.SetValue(winreg.HKEY_LOCAL_MACHINE, subkey, winreg.REG_SZ, manifest)
-
+subkey = r'SOFTWARE\Google\Chrome\NativeMessagingHosts\zrpa.chrome.bridge'
+winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, subkey)
+winreg.SetValue(winreg.HKEY_LOCAL_MACHINE, subkey,
+                winreg.REG_SZ, os.getcwd() + "\manifest.json")
